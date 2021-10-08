c3cd77edf50db78d93b759a32a8f84002b6e349b
LuckyBastard15 <85241539+LuckyBastard15@users.noreply.github.com>
2021-10-07 19:29:05 -0500

    new

--- src/Game.cc
+++ src/Game.cc
@@ -1,8 +1,15 @@
 #include"Game.hh"
 #include"Constants.hh"
 #include"Rectangle.hh"
+#include<InputSystem.hh>
+#include<iostream>
 
 Rectangle* rectangle{new Rectangle(100, 100, 200, 100, sf::Color::Red)};
+sf::Clock* gameClock{new sf::Clock()};
+float deltaTime{};
+sf::Texture* texture1{new sf::Texture()};
+sf::Sprite* sprite1{new sf::Sprite()};
+
 
 Game::Game()
 {
@@ -12,7 +19,14 @@ Game::Game()
 
 void Game::Start()
 {
-
+    texture1->loadFromFile("assets/tiles3.png");
+    if(texture1 != nullptr) std::cout<<"ok";
+    sprite1->setTexture(*texture1);
+    sprite1->setTextureRect(sf::IntRect(0 * 16, 5 * 16, 16, 16));
+    sprite1->setPosition(100, 25);
+    sprite1->setColor(sf::Color::White);
+    sprite1->scale(4.f, 4.f);
+    
 }
 
 Game::~Game()
@@ -29,7 +43,10 @@ void Game::Initialize()
 void Game::Update()
 
 {     
- 
+    deltaTime = gameClock->getElapsedTime().asSeconds();
+    gameClock->restart();
+    //std::cout<< "elapsed time: " << gameClock->getElapsedTime().asMilliseconds() << std::endl;
+
 }
 
 void Game::MainLoop()
@@ -61,12 +78,13 @@ void Game::Render()
 void Game::Draw()
 {
     window->draw(*rectangle->GetShape());
+    window->draw(*sprite1);
 }
 
 void Game::Input()
 
 {  
-
+    rectangle->GetShape()->move(sf::Vector2f(InputSystem::Axis().x * deltaTime * 600.f, InputSystem::Axis().y * deltaTime * 600.f));
 }
 
 void Game::Destroy()
